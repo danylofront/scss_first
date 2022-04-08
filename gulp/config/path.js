81244d9c7fa6cfcb44d39bbb18bb6c58c50e8087
Dany <dany@MacBook-Pro-1.local>
2022-04-08 10:44:52 +0200

    full project

---
+++ gulp/config/path.js
@@ -0,0 +1,37 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = './dist',
+      srcFolder = './src';
+
+export const path = {
+    build: {
+        js: `${buildFolder}/js/`,
+        images: `${buildFolder}/img/`,
+        css: `${buildFolder}/css/`,
+        html: `${buildFolder}/`,
+        fonts: `${buildFolder}/fonts/`,
+        files: `${buildFolder}/files/`
+    },
+    src: {
+        js: `${srcFolder}/js/main.js`,
+        images: `${srcFolder}/img/**/*.{jpg, jpeg, png, gif, webp}`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        scss: `${srcFolder}/scss/style.scss`,
+        html: `${srcFolder}/*.html`,
+        files: `${srcFolder}/files/**/*.*`
+    },
+    watch: {
+        js: `${srcFolder}/js/**/*.js`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        html: `${srcFolder}/**/*.html`,
+        images: `${srcFolder}/img/**/*.{jpg, jpeg, png, svg, gif, ico, webp}`,
+        files: `${srcFolder}/files/**/*.*`
+    },
+
+    clean: buildFolder,
+    buildFolder: buildFolder, 
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+    ftp: ``
+}
